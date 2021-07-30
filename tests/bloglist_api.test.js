e48d5f6eca849bf2bbde12c4413e458e9167b27c
Tim Yu <yuyu.tim@gmail.com>
2021-07-29 17:58:32 -0700

    organized API tests

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -15,90 +15,94 @@ beforeEach(async () => {
   }
 })
 
-test('returns blog posts in JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('retrieving blog posts', () => {
+  test('returns blog posts in JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns the correct number of blog posts', async () => {
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.data.length)
+  })
+
+  test('identifier property is located in "id" of blog posts', async () => {
+    const res = await api.get('/api/blogs')
+    for (let blog of res.body) {
+      expect(blog.id).toBeDefined()
+    }
+  })
 })
 
-test('returns the correct number of blog posts', async () => {
-  const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.data.length)
-})
+describe('saving posts to database', () => {
+  test('saves blog post to database', async () => {
+    const newBlog = {
+      title: 'Example Blog',
+      author: 'Foo Bar',
+      url: 'https://example.com',
+      likes: 3
+    }
 
-test('identifier property is located in "id" of blog posts', async () => {
-  const res = await api.get('/api/blogs')
-  for (let blog of res.body) {
-    expect(blog.id).toBeDefined()
-  }
-})
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('saves blog post to database', async () => {
-  const newBlog = {
-    title: 'Example Blog',
-    author: 'Foo Bar',
-    url: 'https://example.com',
-    likes: 3
-  }
+    const finalBlogs = await helper.blogsInDb()
 
-  await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    expect(finalBlogs)
+      .toHaveLength(helper.data.length + 1)
+    expect(finalBlogs.map(helper.removeExtraneousProperties))
+      .toContainEqual(newBlog)
+  })
 
-  const finalBlogs = await helper.blogsInDb()
+  test('defaults "likes" property to 0 if missing from request', async () => {
+    const newBlog = {
+      title: 'Example Blog',
+      author: 'Foo Bar',
+      url: 'https://example.com'
+    }
 
-  expect(finalBlogs)
-    .toHaveLength(helper.data.length + 1)
-  expect(finalBlogs.map(helper.removeExtraneousProperties))
-    .toContainEqual(newBlog)
-})
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('defaults "likes" property to 0 if missing from request', async () => {
-  const newBlog = {
-    title: 'Example Blog',
-    author: 'Foo Bar',
-    url: 'https://example.com'
-  }
+    const finalBlogs = await helper.blogsInDb()
 
-  await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    expect(finalBlogs)
+      .toHaveLength(helper.data.length + 1)
+    expect(finalBlogs.map(helper.removeExtraneousProperties))
+      .toContainEqual({ ...newBlog, likes: 0 })
+  })
 
-  const finalBlogs = await helper.blogsInDb()
+  test('responds with 400 if title is not in request', async () => {
+    const newBlog = {
+      author: 'Foo Bar',
+      url: 'https://example.com',
+      likes: 3
+    }
 
-  expect(finalBlogs)
-    .toHaveLength(helper.data.length + 1)
-  expect(finalBlogs.map(helper.removeExtraneousProperties))
-    .toContainEqual({ ...newBlog, likes: 0 })
-})
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('responds with 400 if title is not in request', async () => {
-  const newBlog = {
-    author: 'Foo Bar',
-    url: 'https://example.com',
-    likes: 3
-  }
+  test('responds with 400 if url is not in request', async () => {
+    const newBlog = {
+      title: 'Example Blog',
+      author: 'Foo Bar',
+      likes: 3
+    }
 
-  await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('responds with 400 if url is not in request', async () => {
-  const newBlog = {
-    title: 'Example Blog',
-    author: 'Foo Bar',
-    likes: 3
-  }
-
-  await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
 afterAll(() => {
